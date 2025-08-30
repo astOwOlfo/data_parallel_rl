c3a51f63a995ecb098e19ab9bc6d53b05a9a0a0b
astOwOlfo <volodimir1024@gmail.com>
2025-08-29 18:11:32 -0700

    added info print

--- data_parallel_grpo.py
+++ data_parallel_grpo.py
@@ -1003,6 +1003,7 @@ async def grpo_train_process(
 
     dist.barrier()
 
+    with PrintHowLongItTakes("initializing HuggingFace transformer for training")
     training_model = make_training_model(rank=rank, cfg=cfg)
     optimizer = make_optimizer(model=training_model, cfg=cfg)
 
