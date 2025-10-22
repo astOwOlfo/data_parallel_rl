7c5a03f369933082ae62d77d9c0c5506284115c7
Vladimir Ivanov <96141489+astOwOlfo@users.noreply.github.com>
2025-10-22 18:28:09 +0100

    dev

--- data_parallel_grpo.py
+++ data_parallel_grpo.py
@@ -388,7 +388,7 @@ async def generate_rollouts_async(
     )
 
     with PrintHowLongItTakes("Initializing vllm engine for inference"):
-        vllm_engine: LLM = make_vllm_engine(wold_size=world_size, cfg=cfg)
+        vllm_engine: LLM = make_vllm_engine(world_size=world_size, cfg=cfg)
 
     outputs: list[RequestOutput] = vllm_engine.chat(
         messages=prompts,
