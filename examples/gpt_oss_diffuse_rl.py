c26f5b9e65bf030344865ebf3bee6232cba181ae
astOwOlfo <volodimir1024@gmail.com>
2025-10-27 21:41:03 +0000

    dev

--- examples/gpt_oss_diffuse_rl.py
+++ examples/gpt_oss_diffuse_rl.py
@@ -221,7 +221,7 @@ def main():
             use_wandb=True,
             vllm_sleep=True,
             compile_huggingface_model=False,
-            huggingface_model_kwargs={"attn_implementation": "eager", "dtype": torch.bfloat16}
+            huggingface_model_kwargs={"attn_implementation": "eager", "dtype": torch.bfloat16},
             vllm_kwargs={"gpu_memory_utilization": 0.2, "max_model_len": 2048},
             vllm_sampling_params={"max_tokens": 1500, "temperature": 1.0},
             restart_vllm_with_merged_lora=True,
