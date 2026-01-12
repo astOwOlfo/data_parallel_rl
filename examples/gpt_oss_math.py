6d95c1e1a8fe86794d80c86baf97027a2b98e244
astOwOlfo <volodimir1024@gmail.com>
2026-01-12 01:31:45 +0000

    update gpt os math run config

--- examples/gpt_oss_math.py
+++ examples/gpt_oss_math.py
@@ -97,7 +97,6 @@ def main():
             compile_huggingface_model=True,
             vllm_kwargs={"gpu_memory_utilization": 0.5, "max_model_len": 12288},
             vllm_sampling_params=SamplingParams(max_tokens=8192, temperature=1.0),
-            restart_vllm_with_merged_lora=True,
             optimizer_kwargs={"lr": 5e-5},
             lora_rank=128,
             lora_kwargs={"lora_alpha": 32, "target_modules": "all-linear"},
